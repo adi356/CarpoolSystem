603c8c0ec77cf172b5d7f9fab603effd5700733f
Andrew <adi356@mail.umsl.edu>
2024-08-29 11:50:24 -0600

    Initial commit

---
+++ src/main/java/com/airvine/CarpoolSystem/CarpoolSystemApplication.java
@@ -0,0 +1,13 @@
+package com.airvine.CarpoolSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airvine/CarpoolSystem/CarpoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airvine.CarpoolSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
